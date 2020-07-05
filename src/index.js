8f5256d7cf024abdef80fa64d87cd3133d3ae264
Michael Bragg <michael_bragg99@hotmail.com>
2020-07-05 14:28:09 -0600

    Add redux-thunk and applyMiddleware to project.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
 import App from './Components/App/App.jsx';
 import reducers from './reducers';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(reducers, {});
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
   <Provider store={store}>
