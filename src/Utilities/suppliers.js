f936231ff9161ab5ad1ab549b934af1d4c91dd99
Michael Bragg <michael_bragg99@hotmail.com>
2020-08-21 11:25:34 -0600

    Reconfigure supplier fetching.

--- src/Utilities/suppliers.js
+++ src/Utilities/suppliers.js
@@ -1,26 +1,37 @@
+import { fetchSuppliers } from "../../../vendor-editor/src/actions";
+
 const suppliers = (function(){
 
   let suppliers = [];
 
-  return {
-    initializeSuppliers: async function(maxSuppliers) {
-      let initSuppliers;
+  async const fetchSuppliers = function() {
+    let initSuppliers;
       try {
         initSuppliers = await fetch('/suppliers');
       } catch (err) {
         console.log(err);
       }
       if (initSuppliers) {
-        console.log(initSuppliers);
         initSuppliers = initSuppliers.json();
-        console.log(initSuppliers);
-        while (suppliers.length < maxSuppliers && initSuppliers.length > 0) {
-          const pushIndex = Math.floor(Math.random() * initSuppliers.length);
-          console.log(`pushIndex: ${pushIndex}`);
-          let newSupplier = initSuppliers.splice(pushIndex, 1);
-          suppliers.push(newSupplier);
-        }
       }
+      return initSuppliers;
+  }
+
+  return {
+    initializeSuppliers: async function(maxSuppliers) {
+      fetchSuppliers()
+        .then(initSuppliers => {
+          if (initSuppliers) {
+            console.log(initSuppliers);
+            let drainSuppliers = initSuppliers;
+            while (suppliers.length < maxSuppliers && drainSuppliers.length > 0) {
+              const pushIndex = Math.floor(Math.random() * drainSuppliers.length);
+              console.log(`pushIndex: ${pushIndex}`);
+              let newSupplier = drainSuppliers.splice(pushIndex, 1);
+              suppliers.push(newSupplier);
+            }
+          }
+        });
       
       console.log(suppliers);
     },
