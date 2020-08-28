1729df30cced4e76084edca90b7e3cc2bfacd9d0
Michael Bragg <michael_bragg99@hotmail.com>
2020-08-28 04:51:31 -0600

    Add more logs to markup flow.

--- src/game_modules/storeInventory.js
+++ src/game_modules/storeInventory.js
@@ -41,6 +41,8 @@ const storeInventory = (function(){
 
   const markupFilteredItems = function(filter, newMarkup) {
     inventory.forEach(inventoryItem => {
+      console.log(gameItems.getItem(inventoryItem).type);
+      console.log(gameItems.getItemType(inventoryItem.id));
       if (filter.toLowerCase() === 'all' || gameItems.getItemType(inventoryItem.id) === filter) {
         inventoryItem.markup += newMarkup;
         if (inventoryItem.markup > maxMarkup) {
