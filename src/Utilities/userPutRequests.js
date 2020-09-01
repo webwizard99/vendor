a8a74e0a665e700665f4e9a30fe50a80f87f7687
Michael Bragg <michael_bragg99@hotmail.com>
2020-09-01 12:24:39 -0600

    Update userPutRequests utility module.

---
+++ src/Utilities/userPutRequests.js
@@ -0,0 +1,24 @@
+const userPutRequests = (function(){
+  return {
+    makeRequest: async function(putRoute, putBody) {
+      let subBody = {};
+      for ( let [ key, value ] of putBody) {
+        subBody[key] = value
+      }
+
+      subBody = JSON.stringify(subBody)
+
+      const putRes = fetch(`/${putRoute}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: subBody
+      });
+
+      return putRes;
+    }
+  }
+}());
+
+export default userPutRequests;
