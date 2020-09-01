09c858326bcfe5d0849e77adde01457c212920e9
Michael Bragg <michael_bragg99@hotmail.com>
2020-09-01 09:17:49 -0600

    Add styles and close button to ProfileViewer component.

--- src/Containers/ProfileViewer/ProfileViewer.jsx
+++ src/Containers/ProfileViewer/ProfileViewer.jsx
@@ -1,14 +1,48 @@
 import React from 'react';
 import './ProfileViewer.css';
 
+// redux imports
+import { connect } from 'react-redux';
+import { SET_PROFILE_ACTIVE } from '../../actions/types';
+
 class ProfileViewer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  handleClose() {
+    if (this.props.profileActive) {
+      this.props.setProfileActive(false);
+    }
+  }
+  
   render() {
     return (
       <div className="ProfileViewer">
-        ProfileViewer
+        <div className="ProfileForm">
+
+        </div>
+        <div className="CloseButton"
+          onClick={this.handleClose}>
+            X
+        </div>
       </div>
     )
   }
 }
 
-export default ProfileViewer;
+const mapStateToProps = state => {
+  return {
+    profileActive: state.profile.active
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    setProfileActive: (value) => dispatch({ type: SET_PROFILE_ACTIVE, value: value })
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProfileViewer);
