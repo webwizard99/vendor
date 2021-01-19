55387e9b320b7294ab9bc1f4f6e5fdffc9051c12
Michael Bragg <michael_bragg99@hotmail.com>
2021-01-19 06:26:12 -0700

    Add state to track initialization in Detail component.

--- src/Components/Details/Details.jsx
+++ src/Components/Details/Details.jsx
@@ -14,6 +14,10 @@ class Details extends React.Component {
   constructor(props) {
     super(props);
 
+    state = {
+      initialized: false
+    }
+
     this.componentDidMount = this.componentDidMount.bind(this);
     this.loadFormTable = this.loadFormTable.bind(this);
     this.getDetail = this.getDetail.bind(this);
@@ -23,6 +27,7 @@ class Details extends React.Component {
 
   componentDidMount() {
     this.loadFormTable();
+    this.setState({ initialized: true });
   }
 
   loadFormTable() {
@@ -37,6 +42,7 @@ class Details extends React.Component {
   }
 
   getDetail() {
+    if (!this.state.initialized) return ''
     if (!this.props.pcDetailMenu) {
       return (<div className="BlankForm">no details to display</div>);
     }
