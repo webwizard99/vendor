60d4ff148d4bb275ddde2812888d0f9d16938e5b
Michael Bragg <michael_bragg99@hotmail.com>
2021-01-19 07:41:18 -0700

    Update destructuring of adventurer class.

--- src/Components/Adventurer/Adventurer.jsx
+++ src/Components/Adventurer/Adventurer.jsx
@@ -6,13 +6,15 @@ class Adventurer extends React.Component {
   render() {
     if (!this.props.adventurer) return '';
     const adventurer = this.props.adventurer;
-    const classIcon = adventurer.adventurer_class.name.charAt(0);
+    const adventurerClass = adventurer.adventurer_class;
+    console.log(adventurerClass);
+    const classIcon = adventurerClass.name.charAt(0);
     return (
       <div className="adventurerDisplay light">
         <div className="adventurerClassIcon">{classIcon}</div>
         <div className="adventurerDetails">
           <p className="adventurerName">{adventurer.name}</p>
-          <p className="adventurerClass">{adventurer.adventurer_class.name}</p>
+          <p className="adventurerClass">{adventurerClass.name}</p>
           <div className="adventurerStatGroup">
             <p className="adventurerStatLabel">str</p>
             <p className="adventurerStat">{adventurer.strength}</p>
