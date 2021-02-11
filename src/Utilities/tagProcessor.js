cd696b705154e80d823fda050fe73fea64b25567
Michael Bragg <michael_bragg99@hotmail.com>
2021-02-10 19:08:01 -0700

    Update combat log styles.

--- src/Utilities/tagProcessor.js
+++ src/Utilities/tagProcessor.js
@@ -1,49 +1,14 @@
 const tagProcessor = (function(){
-  const tags = {
-    nameStart: '%name%',
-    nameEnd: '%endname%',
-    statusStart: '%status%',
-    statusEnd: '%statusend%'
-  }
 
   const filterClasses = {
-    name: 'adventurerName',
+    name: 'adventurerNameTag',
     status: 'status'
   }
 
-  // const tagArray = Object.values(tags);
-  // const tagKeys = Object.keys(tags);
-
-  // const tagifyStringArray = function(strArr) {
-  //   let found = false;
-  //   let tagIndex = 0;
-  //   let stringIndex = 0;
-  //   const tagTerminate = tagArray.length -1;
-  //   while (!found) {
-  //     found = true;
-  //     const thisTag = tagArray[tagIndex];
-  //     let thisString = strArr[stringIndex];
-  //     if (thisString.find(thisTag)) {
-  //       let tagLocation = thisString.indexOf(thisTag);
-  //       if (tagLocation === 0) {
-
-  //       }
-  //     }
-  //   }
-  // }
-
   return {
-    getTags: function() {
-      return tags;
-    },
     getFilterClasses: function() {
       return filterClasses;
     }
-    // processString: function(str) {
-    //   let arrTag = [str];
-    //   const taggedArray = tagifyString(arrTag);
-    //   return taggedArray;
-    // }
   }
 }());
 
